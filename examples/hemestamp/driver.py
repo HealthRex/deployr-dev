e6723d8d0a4e14cf0ceabfc7596c44eaf377ea0c
conorkcorbin <ccorbin@stanford.edu>
2022-10-20 12:02:16 -0700

    hemestamp driver.py updated in examples

--- examples/hemestamp/driver.py
+++ examples/hemestamp/driver.py
@@ -1,65 +1,46 @@
 """
-Lab predictions and censoring
+Driver to train a hemestamp model
+ex: python driver.py --featurize --train
 """
 
+
+# Authenticate to gcp project [TODO -- change to point to your credentials file]
 import argparse
 from google.cloud import bigquery
+import json
 import os
 import pandas as pd
 import sys
-import json
-
 from healthrex_ml.cohorts import *
 from healthrex_ml.trainers import *
 from healthrex_ml.featurizers import *
-
-# [EDIT] Authenticate to gcp project 
+from healthrex_ml.extractors import *
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = (
-    '/home/conorcorbin/.config/gcloud/application_default_credentials.json'
+    '/Users/conorcorbin/.config/gcloud/application_default_credentials.json'
 )
 os.environ['GCLOUD_PROJECT'] = 'som-nero-phi-jonc101'
-
 client = bigquery.Client()
+
 parser = argparse.ArgumentParser(description='Build cohorts, featurize, train')
 parser.add_argument(
-    '--run_name',
-    default='20220915_hemestamp_run',
-    help='Ex: date of execution'
-)
-parser.add_argument(
-    '--working_project_id',
-    default='mining-clinical-decisions',
-    help='Project id for temp tables'
-)
-parser.add_argument(
-    '--working_dataset_name',
-    default='conor_db',
-    help='Bigquery dataset name where we store cohorts and features'
-)
-parser.add_argument(
-    '--project_id',
-    default='som-nero-phi-jonc101',
-    help='project id that holds dataset used to create cohort and features'
-)
-parser.add_argument(
-    '--dataset',
-    default='shc_core_2021',
-    help='dataset used to create cohort and features'
-)
-parser.add_argument(
-    '--cohort_table_name',
-    default='20220811_hemestamp_cohort',
-    help='ex: just table name, project and dataset handled above'
-)
-parser.add_argument(
-    '--feature_table_name',
-    default='feature_matrix',
-    help='just table name'
+    '--experiment_name',
+    default='20221020',
+    help='Experiment name, prefix of all saved tables'
 )
 parser.add_argument(
     '--outpath',
     default='model_info',
     help='Out directory to write files to'
+)
+parser.add_argument(
+    '--extractors',
+    nargs='+',
+    default=['AgeExtractor', 'RaceExtractor', 'EthnicityExtractor',
+             'SexExtractor', 'PatientProblemExtractor',
+             'LabOrderExtractor', 'ProcedureExtractor',
+             'MedicationExtractor', 'LabResultBinsExtractor',
+             'FlowsheetBinsExtractor'],
+    help='List of extactors to use when'
 )
 parser.add_argument(
     '--featurizer',
@@ -69,75 +50,107 @@ parser.add_argument(
 parser.add_argument(
     '--trainer',
     default='LightGBMTrainer',
-    help='Trainer to use'
+    help='Trainer to use (type of model)'
 )
 parser.add_argument(
-    "--tasks",
-    default=['label_hemestamp'],  # set of tasks (name of label column)
-    nargs="*",
-)
-parser.add_argument(
-    '--tfidf',
+    '--featurize',
     action='store_true',
-    help='whether to transform bow with tfidf'
+    help='Whether to featurize'
+)
+parser.add_argument(
+    '--train',
+    action='store_true',
+    help='Whether to train models'
 )
 args = parser.parse_args()
 
-args_dict =  vars(args)
-os.makedirs(f"./runs/{args.run_name}_{args.outpath}", exist_ok=True)
-with open(f"./runs/{args.run_name}_{args.outpath}/command_args.json", 'w') as f:
-    json.dump(args_dict, f)
+# Dump args to json
+outpath = f"./{args.experiment_name}_hemestamp_{args.outpath}"
+os.makedirs(outpath, exist_ok=True)
+with open(os.path.join(outpath, 'args.json'), 'w') as f:
+    json.dump(vars(args), f)
 
-# Feature config
-FEATURE_CONFIG = {
-    'Categorical': {
-        'Sex': [{'look_back': None}],
-        # 'Race': [{'look_back': None}],
-        # 'Diagnoses': [{'look_back': None}], # None implies infinite look back
-        # 'Medications': [{'look_back': 28}],
-        # 'Procedures': [{'look_back': 28}]
-
-    },
-    'Numerical': {
-        'Age': [{'look_back': None, 'num_bins': 5}],
-        'LabResults': [{'look_back': 180, 'num_bins': 5}],
-        # 'Vitals': [{'look_back': 3, 'num_bins': 5}]
-    }
-}
-
-# Possible cohorts, featurizers and trainers
+# Create dictionary of cohorts, featurizers and trainers
 featurizers = {
     'BagOfWordsFeaturizer': BagOfWordsFeaturizer,
 }
 trainers = {
     'LightGBMTrainer': LightGBMTrainer,
-    'BaselineModelTrainer': BaselineModelTrainer
 }
 
-cohort_table = (f"{args.working_project_id}.{args.working_dataset_name}."
-                 f"{args.cohort_table_name}")
-feature_table = (f"{args.working_project_id}.{args.working_dataset_name}."
-                 f"{args.run_name}_{args.feature_table_name}")
+cohort_table_id = f"mining-clinical-decisions.conor_db.20220811_hemestamp_cohort"
+feature_table_id = f"mining-clinical-decisions.benchmarking.{args.experiment_name}_hemestamp_features"
 
-if args.featurizer == 'BagOfWordsFeaturizer':
-    featurizer = BagOfWordsFeaturizer(
-        cohort_table_id=cohort_table,
-        feature_table_id=feature_table,
-        outpath=f"./runs/{args.run_name}_{args.outpath}",
-        project=args.project_id,
-        dataset=args.dataset,
-        tfidf=args.tfidf,
-        feature_config=FEATURE_CONFIG
+# Create list of extactors
+extractors = []
+if 'AgeExtractor' in args.extractors:
+    extractors.append(AgeExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'RaceExtractor' in args.extractors:
+    extractors.append(RaceExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'SexExtractor' in args.extractors:
+    extractors.append(SexExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'EthnicityExtractor' in args.extractors:
+    extractors.append(EthnicityExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'PatientProblemExtractor' in args.extractors:
+    extractors.append(PatientProblemExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'LabOrderExtractor' in args.extractors:
+    extractors.append(LabOrderExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'ProcedureExtractor' in args.extractors:
+    extractors.append(ProcedureExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'MedicationExtractor' in args.extractors:
+    extractors.append(MedicationExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id
+    ))
+if 'LabResultBinsExtractor' in args.extractors:
+    extractors.append(LabResultBinsExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id,
+        base_names=DEFAULT_LAB_COMPONENT_IDS
+    ))
+if 'FlowsheetBinsExtractor' in args.extractors:
+    extractors.append(FlowsheetBinsExtractor(
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id,
+        base_names=DEFAULT_FLOWSHEET_FEATURES
+    ))
+
+if args.featurize:
+    print("Featurizing")
+    featurizer = featurizers[args.featurizer](
+        cohort_table_id=cohort_table_id,
+        feature_table_id=feature_table_id,
+        extractors=extractors,
+        outpath=f"./{args.experiment_name}_hemestamp_{args.outpath}",
+        tfidf=True
     )
-    print("Creating features")
-    featurizer()  # Call to featurizer
+    featurizer()
 
 # Train model
-if args.trainer is not None:
+if args.train:
     print("Training")
-
     trainer = trainers[args.trainer](
-        working_dir=f"./runs/{args.run_name}_{args.outpath}"
+        working_dir=f"./{args.experiment_name}_hemestamp_{args.outpath}"
     )
-    for task in args.tasks:
-        trainer(task)
+    trainer(task='label_hemestamp')
