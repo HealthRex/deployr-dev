560f5280b87482134612f30a867ccb5b5a8c22bb
brosular <brosular@stanford.edu>
2022-11-28 08:40:25 +0000

    Updated index mapping and command generation

--- examples/benchmarks/wrapper.py
+++ examples/benchmarks/wrapper.py
@@ -3,6 +3,7 @@ Wrapper to execute various driver.py commands
 """
 from itertools import chain, combinations
 import json
+import os
 
 #Extract all possible extractor subsets
 extractors = [['AgeExtractor', 'RaceExtractor', 'EthnicityExtractor',
@@ -12,6 +13,7 @@ extractors = [['AgeExtractor', 'RaceExtractor', 'EthnicityExtractor',
              ['FlowsheetBinsExtractor']]
 
 extractor_powerset = list(chain.from_iterable(combinations(extractors, r) for r in range(1, len(extractors) + 1)))
+extractor_map_powerset = list(chain.from_iterable(combinations(range(1, len(extractors) + 1), r) for r in range(1, len(extractors) + 1)))
 
 #Flatten lists
 def convertElements(lst):
@@ -23,26 +25,29 @@ def convertElements(lst):
     return new_lst
 
 extractor_subsets = convertElements(extractor_powerset)
+extractor_maps = [list(ex_map) for ex_map in extractor_map_powerset]
 
 # Set up mapping between index and extractor subsets and save this mapping
-index = {i : extractor_subsets[i] for i in range(len(extractor_subsets))} 
-index_file = './index.json'
-with open(index_file, 'w') as f:
-    json.dump(index, f)
+index_str = {i : extractor_subsets[i] for i in range(len(extractor_subsets))} 
+index_num = {i : ''.join(str(n) for n in extractor_maps[i]) for i in range(len(extractor_map_powerset))}
+
+with open("./index_str.json", "w") as f:
+    json.dump(index_str, f)
+with open("./index_num.json", "w") as f:
+    json.dump(index_num, f)
 
 # Set up commands
 commands = []
-for index, subset in index.items():
-    subset_command = "python driver.py --build_cohort --cohort InpatientMortalityCohort " + \
-    "--extract --featurize --train --evaluate --evaluator BinaryEvaluatorByTime "
-    subset_command += f"--experiment_name 20221110_subset{index} "
+for index, subset in index_str.items():
+    subset_command = "python driver.py --cohort ThirtyDayReadmission " + \
+    "--featurize --train --evaluate --evaluator BinaryEvaluatorByTime "
+    subset_command += f"--outpath ./20221115__subset{index} "
+    subset_command += f"--experiment_name 20221115_readmit "
     extractors_str = "--extractors " + ' '.join(subset)
     subset_command += extractors_str
-    subset_command += " &"
+    subset_command += f" > ./log/20221115_readmit_subset{index} 2>> ./log/20221115_readmit_subset{index} &"
     commands.append(subset_command)
 
-for command in commands:
-    print(command)
-# Execute the commands [TODO]
-# for command in commands:
-#     os.system(command)
+#Execute the commands
+for i in range(len(commands)):
+    os.system(commands[i])
