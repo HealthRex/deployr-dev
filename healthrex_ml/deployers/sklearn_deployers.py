d53858ef392b82cdd34ccb080722b0c2c1cfe365
conorkcorbin <ccorbin@stanford.edu>
2023-04-06 13:05:03 -0700

    secrets naming convention

--- healthrex_ml/deployers/sklearn_deployers.py
+++ healthrex_ml/deployers/sklearn_deployers.py
@@ -98,7 +98,7 @@ class SklearnDeployer(object):
                                data.keys()} if isinstance(data, dict) else 
                                data.tolist()}
         headers = {
-            'Authorization': f'Bearer {os.environ.get("MLFLOW-TOKEN")}',
+            'Authorization': f'Bearer {os.environ.get("MLFLOWTOKEN")}',
             'Content-Type': 'application/json'
         }
         ds_dict = self.feature_vector.to_dict(orient='split') \
